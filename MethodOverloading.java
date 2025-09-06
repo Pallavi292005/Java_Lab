f4580381f359bb2e1f208336cbd52c49c31c2dde
Pallavi_Nawander <24wh1a05y2@bvrithyderabad.edu.in>
2025-09-06 21:04:59 +0530

    Create MethodOverloading.java

---
+++ MethodOverloading.java
@@ -0,0 +1,15 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println(c.add(2, 3));       // int version
+        System.out.println(c.add(2.5, 3.5));   // double version
+    }
+}
